03adc2cc3105a87e1d251b14936d0361c6eafbfc
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-01-16 09:04:17 -0400

    added questions and init function in index.js

--- index.js
+++ index.js
@@ -1,70 +1,136 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-//Questions
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'Manager-name',
-            message: 'What is the team manager name?',
-        },
-        {
-            type: 'input',
-            name: 'Manager-id',
-            message: 'What is the team manager id?',
-        },
-        {
-            type: 'input',
-            name: 'manager-email',
-            message: 'What is the team manager email?',
-        },
-        {
-            type: 'input',
-            name: 'manager-office-number',
-            message: 'What is the team manager office number?',
-        },
-        {
-            type: 'list',
-            name: 'team',
-            message: 'Which type of team memeber would you like to add?',
-            choices: ['Engineer', 'Intern', 'I dont want to add any more team members'],
-        },
-        {
-            when: (answers) => answers['team'] == 'Intern',
-            type: 'Input',
-            name: 'Intern-name',
-            message: 'What is your intern name?',
+// Classes
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
 
-        },
-        {
-            when: (answers) => answers['team'] == 'Intern',
-            type: 'input',
-            name: 'Intern-id',
-            message: 'What is your Intern id?',
-        },
-        {
-            when: (answers) => answers['team'] == 'Intern',
-            type: 'input',
-            name: 'Intern-email',
-            message: 'What is your Intern email?',
-        },
-        {
-            when: (answers) => answers['team'] == 'Intern',
-            type: 'input',
-            name: 'Intern-school',
-            message: 'What is your Intern school?',
-        },
-        {
-            when: (answers) => answers['team'] == 'Engineer',
-            type: 'input',
-            name: 'Engineer-name',
-            message: 'What is your Engineer name?',
-        },
-    ])
+
+
+let teamArr = [];
+
+//Questions
+const questions = [
+    {
+        type: 'list',
+        name: 'team',
+        message: 'Which type of team memeber would you like to add?',
+        choices: ['Manager', 'Engineer', 'Intern', 'I dont want to add any more team members'],
+    }
+];
+
+const manager = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the team manager name?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the team manager id?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the team manager email?',
+    },
+    {
+        type: 'input',
+        name: 'office-number',
+        message: 'What is the team manager office number?',
+    }
+];
+
+
+const engineer = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your Engineer name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the engineer id?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the engineer email?',
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is the engineer gitHub?',
+    },
+
+];
+
+
+const intern = [
+    {
+        when: (answers) => answers['team'] == 'Intern',
+        type: 'Input',
+        name: 'name',
+        message: 'What is your intern name?',
+
+    },
+    {
+        when: (answers) => answers['team'] == 'Intern',
+        type: 'input',
+        name: 'id',
+        message: 'What is your Intern id?',
+    },
+    {
+        when: (answers) => answers['team'] == 'Intern',
+        type: 'input',
+        name: 'email',
+        message: 'What is your Intern email?',
+    },
+    {
+        when: (answers) => answers['team'] == 'Intern',
+        type: 'input',
+        name: 'school',
+        message: 'What is your Intern school?',
+    },
+];
+
+
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        if (answers.team === 'I dont want to add any more team members') {
+            console.log(teamArr);
+            return;
+        }
+        if (answers.team === 'Manager') {
+            inquirer.prompt(manager).then(answers => {
+                const manager = new Manager(answers.name, answers.id, answers.email, answers.office - number)
+                teamArr.push(manager);
+                init()
+            })
+        }
+
+    })
 }
 
-promptUser()
-    .then(answers => {
-        console.log(answers)
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
